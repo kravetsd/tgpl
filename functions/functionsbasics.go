990807d56abe3b868a3edee8c0c0cb7dc699c305
kravetsd <kdykrg@gmail.com>
2021-11-13 16:23:17 +0300

    added gitignore

--- functions/functionsbasics.go
+++ functions/functionsbasics.go
@@ -7,3 +7,8 @@ import (
 func Hellofunc() {
 	fmt.Println("Hello from functions")
 }
+
+func Hypot() {
+	fmt.Println(`this is a simple hypot function.\n 
+	"hypot" means that the function itself doesn't returns anything and is needed only for making some actions`)
+}
--- main.go
+++ main.go
@@ -9,4 +9,5 @@ import (
 func main() {
 	fmt.Println("Hello module")
 	functions.Hellofunc()
+	functions.Hypot()
 }
