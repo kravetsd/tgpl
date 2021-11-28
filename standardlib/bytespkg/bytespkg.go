168a58bd877a80f94af4665060a202796248e7ba
kravetsd <kdykrg@gmail.com>
2021-11-29 01:03:01 +0300

    [stflib]:bytes:interesting bytes functions

--- standardlib/bytespkg/bytespkg.go
+++ standardlib/bytespkg/bytespkg.go
@@ -1,6 +1,7 @@
 package bytespkg
 
 import (
+	"bytes"
 	"fmt"
 	"unicode/utf8"
 )
@@ -28,7 +29,18 @@ func BasicEncoding() {
 
 	fmt.Println(utf8.RuneCountInString(s))
 
+	a := '`'
+	fmt.Printf("%q\n", a)
 	for i, val := range "Hello, ◺ this is string" {
 		fmt.Printf("i: %v\t val: %d\t letter: %q\t len: %d\t %c\n", i, val, val, utf8.RuneLen(val), val)
 	}
+
+	fmt.Println("Triangle is in the", bytes.IndexRune([]byte("Hello, ◺ this is string"), '◺'), "place")
+
+	//fmt.Printf("%#v", bytes.Replace([]byte("Hello, ◺ this is string"), []byte("◺"), []byte("WTF"), -1))
+
+	runes := bytes.Runes([]byte{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x57, 0x54, 0x46, 0x20, 0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x73,
+		0x74, 0x72, 0x69, 0x6e, 0x67})
+
+	fmt.Println(string(runes))
 }
