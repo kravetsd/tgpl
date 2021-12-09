3db1ecdee2325066e2dd6c40461fbdf722fc235e
kravetsd <kdykrg@gmail.com>
2021-12-10 02:07:16 +0300

    [stdlib] ssh package: simple ssh client

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/kravetsd/TheGoProgrammingLanguage
 
 go 1.17
 
-require golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
-
 require (
-	github.com/aws/aws-sdk-go v1.42.15 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20211209193657-4570a0811e8b
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
 )
+
+require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
---
+++ standardlib/ssh/basics.go
@@ -0,0 +1,64 @@
+package ssh
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func sshbasic(user string, remoteAddress, remotePort string, key []byte) {
+
+	// FROM THE DOCUMETNATION PAGE:
+	// An SSH client is represented with a ClientConn.
+	//
+	// To authenticate with the remote server you must pass at least one
+	// implementation of AuthMethod via the Auth field in ClientConfig,
+	// and provide a HostKeyCallback.
+
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		log.Fatal("Error with parsing private key : ", err)
+	}
+
+	config := &ssh.ClientConfig{
+		Config: ssh.Config{},
+		User:   user,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+	}
+
+	// Implementing a new ssh client connection:
+
+	// setting up a connection to temote host
+	c, err := net.Dial("tcp", fmt.Sprintf(remoteAddress+":"+remotePort))
+	if err != nil {
+		log.Fatalf("Can't establich tcp connection to %s : %s", remoteAddress, err)
+	}
+
+	// establishing connection:
+	connection, newchan, requestchan, err := ssh.NewClientConn(c, remoteAddress, config)
+	if err != nil {
+		log.Fatalf("Error to establich ssh connection to %s : %s", remoteAddress, err)
+	}
+	client := ssh.NewClient(connection, newchan, requestchan)
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatalf("Establishing SSH session error: %s", err)
+	}
+	defer session.Close()
+
+	// once session is created we can execute command:
+
+	// creating a buffer for session's output storage
+	var b bytes.Buffer
+	session.Stdout = &b
+
+	if err := session.Run("/usr/bin/whoami"); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+}
